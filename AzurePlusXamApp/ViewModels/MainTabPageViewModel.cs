97a76723e3b4551d5b78638621e5e444f029f1ff
Cesar Contreras <cesar.contrerasm@outlook.com>
2019-04-22 17:15:07 -0400

    Adds MainTabPageVM empty class

---
+++ AzurePlusXamApp/ViewModels/MainTabPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AzurePlusXamApp.Helpers;
+using Prism.Commands;
+using Prism.Navigation;
+
+namespace AzurePlusXamApp.ViewModels
+{
+    public class MainTabPageViewModel : BaseViewModel
+    {
+        public MainTabPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
